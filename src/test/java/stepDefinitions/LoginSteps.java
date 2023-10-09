d79d8b76abbbb33887c481635976a01255e46171
ChatGTHB <cihat.kose@hotmail.com>
2023-10-09 17:51:16 +0200

    Added Apache POI files

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,15 +1,16 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
+import org.apache.poi.ss.usermodel.*;
 import org.testng.Assert;
 import pages.LoginPage;
 import utilities.GWD;
 
+import java.io.*;
+
+
 public class LoginSteps {
-    LoginPage lp=new LoginPage();
+    LoginPage lp = new LoginPage();
 
     @Given("Navigate to website")
     public void navigateToWebsite() {
@@ -31,5 +32,21 @@ public class LoginSteps {
     @Then("Verify that user logged in")
     public void verifyThatUserLoggedIn() {
         Assert.assertTrue(lp.userIcon.isEnabled());
+    }
+
+    @And("Enter username and password from ZeroBankData")
+    public void enterUsernameAndPasswordFromZeroBankData() throws IOException {
+
+        String path = "src/test/java/apachePOI/resource/_ZeroBankData.xlsx";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        Sheet sheet = workbook.getSheet("Sheet1");
+
+        String username = String.valueOf(sheet.getRow(4).getCell(5));
+        String password = String.valueOf(sheet.getRow(4).getCell(6));
+
+        lp.mySendKeys(lp.username, username);
+        lp.mySendKeys(lp.password, password);
     }
 }
