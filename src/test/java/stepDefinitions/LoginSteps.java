9bfc8e06a2ffdd97ab9f7a9e3dd2a9ccc5d6dd63
ChatGTHB <cihat.kose@hotmail.com>
2024-03-27 13:37:03 +0100

    Edited

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -10,7 +10,7 @@ import java.io.*;
 
 
 public class LoginSteps {
-    Login lp = new Login();
+    Login login = new Login();
 
     @Given("Navigate to website")
     public void navigateToWebsite() {
@@ -19,19 +19,19 @@ public class LoginSteps {
 
     @And("Enter username that as {string} and password that as {string}")
     public void enterUsernameThatAsAndPasswordThatAs(String username, String password) {
-        lp.mySendKeys(lp.username, username);
-        lp.mySendKeys(lp.password, password);
+        login.mySendKeys(login.username, username);
+        login.mySendKeys(login.password, password);
     }
 
     @When("Click login button")
     public void clickLoginButton() {
-        lp.myClick(lp.signInButton);
+        login.myClick(login.signInButton);
         GWD.getDriver().navigate().back();
     }
 
     @Then("Verify that user logged in")
     public void verifyThatUserLoggedIn() {
-        Assert.assertTrue(lp.userIcon.isEnabled());
+        Assert.assertTrue(login.userIcon.isEnabled());
     }
 
     @And("Enter username and password from ZeroBankData")
@@ -46,7 +46,7 @@ public class LoginSteps {
         String username = String.valueOf(sheet.getRow(4).getCell(5));
         String password = String.valueOf(sheet.getRow(4).getCell(6));
 
-        lp.mySendKeys(lp.username, username);
-        lp.mySendKeys(lp.password, password);
+        login.mySendKeys(login.username, username);
+        login.mySendKeys(login.password, password);
     }
 }
